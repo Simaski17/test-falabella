25f8d5196d94b921a01f40b65bd1a28c26499c65
Jimmy Hernandez <jimmyhernandezu@gmail.com>
2021-02-27 16:50:28 -0300

    added login and registration layout

--- app/src/main/java/com/example/testfalabella/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/testfalabella/ui/login/LoginFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.testfalabella.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btSignIn.setOnClickListener(this)
+        btRegister.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btSignIn -> {
+            }
+            R.id.btRegister -> {
+                val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+                findNavController().navigate(action)
+            }
+        }
     }
 
 }
